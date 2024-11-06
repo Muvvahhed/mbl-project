4d59e433858c11fbe0c3eae80aa3d96b7059063f
Muwaheed <muwaheedmustapha@gmail.com>
2024-11-06 17:26:35 +0100

    fix email

--- index.js
+++ index.js
@@ -32,3 +32,52 @@ for (let i = 0; i < testimonialCarouselActions.length; i++) {
 		e.target.classList.add('bg-white')
 	})
 }
+
+document
+	.getElementById('contactForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault()
+
+		const buttton = document.getElementById('submit-btn')
+		buttton.textContent = 'Sending...'
+		const formData = new FormData(event.target)
+
+		const fromName = formData.get('user_name')
+		const message = formData.get('user_message')
+		const email = formData.get('user_email')
+		const phone = formData.get('user_phone')
+
+		// Sends the email
+		emailjs
+			.send('service_jh6unt9', 'template_dzi3doa', {
+				from_name: fromName,
+				message: message,
+				email: email,
+				phone: phone,
+				reply_to: email,
+			})
+			.then((response) => {
+				const toast = document.getElementById('toast')
+				const message = document.getElementById('toast-message')
+				message.classList.add('success')
+				message.textContent = 'Email sent successfully'
+				toast.classList.remove('hidden')
+
+				setTimeout(() => {
+					toast.classList.add('hidden')
+				}, 3000)
+			})
+			.catch(() => {
+				const toast = document.getElementById('toast')
+				const message = document.getElementById('toast-message')
+				message.classList.add('error')
+				message.textContent = 'Failed to send email'
+				toast.classList.remove('hidden')
+				setTimeout(() => {
+					document.getElementById('toast').classList.add('hidden')
+				}, 3000)
+			})
+			.finally(() => {
+				buttton.textContent = 'Get Started Now'
+			})
+	})
