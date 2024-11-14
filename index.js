231a02d0f91b1c40c121489ca8622daf0b1ebaff
Muwaheed <muwaheedmustapha@gmail.com>
2024-11-14 12:36:14 +0100

    finish changes

--- index.js
+++ index.js
@@ -1,83 +1,291 @@
-let currentIndex = 0
-const items = document.querySelectorAll('.hero-carousel-item')
-const navbar = document.querySelector('nav')
-const totalItems = items.length
+// let currentIndex = 0
+// const items = document.querySelectorAll('.hero-carousel-item')
+// const navbar = document.querySelector('nav')
+// const totalItems = items.length
 
-function showNextSlide() {
-	const styles = ['bg-primary', 'bg-secondary', 'bg-purple-500']
-	items[currentIndex].classList.remove('opacity-100', 'block')
-	items[currentIndex].classList.add('hidden')
+// function showNextSlide() {
+// 	const styles = ['bg-primary', 'bg-secondary', 'bg-purple-500']
+// 	items[currentIndex].classList.remove('opacity-100', 'block')
+// 	items[currentIndex].classList.add('hidden')
 
-	currentIndex = (currentIndex + 1) % totalItems
+// 	currentIndex = (currentIndex + 1) % totalItems
 
-	items[currentIndex].classList.remove('hidden')
-	items[currentIndex].classList.add('opacity-100', 'block')
+// 	items[currentIndex].classList.remove('hidden')
+// 	items[currentIndex].classList.add('opacity-100', 'block')
 
-	navbar?.classList.remove(...styles)
-	navbar?.classList.add(styles[currentIndex])
-}
+// 	navbar?.classList.remove(...styles)
+// 	navbar?.classList.add(styles[currentIndex])
+// }
 
-setInterval(showNextSlide, 5000)
+// setInterval(showNextSlide, 5000)
 
-const testimonialCarouselActions = document.querySelectorAll(
-	'.testimonial-carousel-button div'
-)
-for (let i = 0; i < testimonialCarouselActions.length; i++) {
-	testimonialCarouselActions[i].addEventListener('click', (e) => {
-		document
-			.querySelectorAll('.testimonial-carousel-button div')
-			.forEach((element) => {
-				element.classList.remove('bg-white')
+// const testimonialCarouselActions = document.querySelectorAll(
+// 	'.testimonial-carousel-button div'
+// )
+// for (let i = 0; i < testimonialCarouselActions.length; i++) {
+// 	testimonialCarouselActions[i].addEventListener('click', (e) => {
+// 		document
+// 			.querySelectorAll('.testimonial-carousel-button div')
+// 			.forEach((element) => {
+// 				element.classList.remove('bg-white')
+// 			})
+// 		e.target.classList.add('bg-white')
+// 	})
+// }
+
+// document
+// 	.getElementById('contactForm')
+// 	.addEventListener('submit', function (event) {
+// 		event.preventDefault()
+
+// 		const buttton = document.getElementById('submit-btn')
+// 		buttton.textContent = 'Sending...'
+// 		const formData = new FormData(event.target)
+
+// 		const fromName = formData.get('user_name')
+// 		const message = formData.get('user_message')
+// 		const email = formData.get('user_email')
+// 		const phone = formData.get('user_phone')
+
+// 		// Sends the email
+// 		emailjs
+// 			.send('service_jh6unt9', 'template_dzi3doa', {
+// 				from_name: fromName,
+// 				message: message,
+// 				email: email,
+// 				phone: phone,
+// 				reply_to: email,
+// 			})
+// 			.then((response) => {
+// 				const toast = document.getElementById('toast')
+// 				const message = document.getElementById('toast-message')
+// 				message.classList.add('success')
+// 				message.textContent = 'Email sent successfully'
+// 				toast.classList.remove('hidden')
+
+// 				setTimeout(() => {
+// 					toast.classList.add('hidden')
+// 				}, 3000)
+// 			})
+// 			.catch(() => {
+// 				const toast = document.getElementById('toast')
+// 				const message = document.getElementById('toast-message')
+// 				message.classList.add('error')
+// 				message.textContent = 'Failed to send email'
+// 				toast.classList.remove('hidden')
+// 				setTimeout(() => {
+// 					document.getElementById('toast').classList.add('hidden')
+// 				}, 3000)
+// 			})
+// 			.finally(() => {
+// 				buttton.textContent = 'Get Started Now'
+// 			})
+// 	})
+
+class NumberAnimator {
+	constructor(element, target, duration = 2000) {
+		this.element = element
+		this.target = target
+		this.duration = duration
+		this.startTime = null
+		this.observer = null
+		this.initObserver()
+	}
+
+	// Initialize Intersection Observer
+	initObserver() {
+		this.observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					this.animate()
+				} else {
+					this.element.textContent = '0'
+				}
 			})
-		e.target.classList.add('bg-white')
-	})
+		})
+		this.observer.observe(this.element)
+	}
+
+	animate() {
+		this.startTime = Date.now()
+		this.updateNumber()
+	}
+
+	updateNumber() {
+		const elapsed = Date.now() - this.startTime
+		const progress = Math.min(elapsed / this.duration, 1)
+		const currentValue = Math.floor(progress * this.target)
+
+		this.element.textContent = currentValue
+
+		if (progress < 1) {
+			requestAnimationFrame(this.updateNumber.bind(this))
+		} else {
+			this.element.textContent = this.target
+		}
+	}
 }
+
+const number1 = document.getElementById('number1')
+const number2 = document.getElementById('number2')
+const number3 = document.getElementById('number3')
+
+const animator1 = new NumberAnimator(number1, 10, 3000)
+const animator2 = new NumberAnimator(number2, 240, 2500)
+const animator3 = new NumberAnimator(number3, 78, 2000)
+
+document.querySelectorAll('.payment-methods-carousel').forEach((element) => {
+	element.innerHTML += element.innerHTML
+	element.innerHTML += element.innerHTML
+	element.innerHTML += element.innerHTML
+})
 
 document
-	.getElementById('contactForm')
-	.addEventListener('submit', function (event) {
-		event.preventDefault()
+	.querySelector('.hero-section')
+	.addEventListener('mousemove', function (e) {
+		const lightEffect = document.querySelector('.light-effect')
+		const rect = this.getBoundingClientRect()
+		const x = e.clientX - rect.left
+		const y = e.clientY - rect.top
 
-		const buttton = document.getElementById('submit-btn')
-		buttton.textContent = 'Sending...'
-		const formData = new FormData(event.target)
-
-		const fromName = formData.get('user_name')
-		const message = formData.get('user_message')
-		const email = formData.get('user_email')
-		const phone = formData.get('user_phone')
-
-		// Sends the email
-		emailjs
-			.send('service_jh6unt9', 'template_dzi3doa', {
-				from_name: fromName,
-				message: message,
-				email: email,
-				phone: phone,
-				reply_to: email,
-			})
-			.then((response) => {
-				const toast = document.getElementById('toast')
-				const message = document.getElementById('toast-message')
-				message.classList.add('success')
-				message.textContent = 'Email sent successfully'
-				toast.classList.remove('hidden')
-
-				setTimeout(() => {
-					toast.classList.add('hidden')
-				}, 3000)
-			})
-			.catch(() => {
-				const toast = document.getElementById('toast')
-				const message = document.getElementById('toast-message')
-				message.classList.add('error')
-				message.textContent = 'Failed to send email'
-				toast.classList.remove('hidden')
-				setTimeout(() => {
-					document.getElementById('toast').classList.add('hidden')
-				}, 3000)
-			})
-			.finally(() => {
-				buttton.textContent = 'Get Started Now'
-			})
+		lightEffect.style.left = `${x}px`
+		lightEffect.style.top = `${y}px`
+		lightEffect.style.opacity = 1
 	})
+
+document
+	.querySelector('.hero-section')
+	.addEventListener('mouseleave', function () {
+		const lightEffect = document.querySelector('.light-effect')
+		lightEffect.style.opacity = 0
+	})
+
+const testimonials = [
+	[
+		{
+			quote:
+				"Everything promised and more! It's rare to find a company that delivers on its word.",
+			name: 'Lana Rey',
+			title: 'Founder & leader',
+			image:
+				'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp',
+		},
+		{
+			quote:
+				'Is it possible to fall in love with a company? Well, I did! They are the best in the business.',
+			name: 'John Doe',
+			title: 'CEO & Founder',
+			image:
+				'https://images.generated.photos/AGajC2_viO_FD9mUUN860fpb8zL_gz_Pw-VC_oX-fw8/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTIyNTQzLmpwZw.jpg',
+		},
+		{
+			quote:
+				"Transparency, honesty, and integrity. That's what you get when you work with this company.",
+			name: 'Chen Li',
+			title: 'Finance Manager',
+			image:
+				'https://images.generated.photos/X6OMUnBl1rlOBnPgTN6LbCR8Jf81eUXbh0Bx0XTZE84/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTA0MTIyLmpwZw.jpg',
+		},
+	],
+	[
+		{
+			quote:
+				'Fastest way to get your business off the ground. They are the best in the business.',
+			name: 'Gabriella Smith',
+			title: 'Marketing Manager',
+			image:
+				'https://images.generated.photos/_6k0_gRXM89EclypTE03kJzSlNvtssinYSczO3R0wxA/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTYwNzI1LmpwZw.jpg',
+		},
+		{
+			quote:
+				'I saved so much in hidden fees. This app is transparent, trustworthy, and super easy to use!',
+			name: 'Mark Johnson',
+			title: 'CTO & Co-Founder',
+			image:
+				'https://images.generated.photos/fcSJuy4iLadsnYQZXOGqcZm_8MP8XqhQw-5p7HeHPko/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTM0OTg5LmpwZw.jpg',
+		},
+		{
+			quote:
+				'Instant transfers, low fees, and top-notch security—what more could you ask for?',
+			name: 'Mahershala Ali',
+			title: 'Actor & Producer',
+			image:
+				'https://images.generated.photos/6Rvff04erBdpLhPXyV-GZE500SIzKI-YCBtlLykuZog/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzYxODI0LmpwZw.jpg',
+		},
+	],
+	[
+		{
+			quote:
+				'Makes sending and receiving money seamless. The best experience I’ve had with any financial app.',
+			name: 'Ranbir Kapoor',
+			title: 'Writer',
+			image:
+				'https://images.generated.photos/yHJL5ymbZeGfnZmnUYlZnOlCXkGdNSFdpIcdtNblBaY/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzMxODI3LmpwZw.jpg',
+		},
+		{
+			quote:
+				'Tracking expenses and investments has never been this fun and simple.',
+			name: 'Cynthia Erivo',
+			title: 'Singer & Songwriter',
+			image:
+				'https://images.generated.photos/F46n-_Vik3phLILcavTBAV3y1U-z6yMvRsP0brbqT1k/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjIxMTQ5LmpwZw.jpg',
+		},
+		{
+			quote:
+				'No more waiting for days to see transactions. This app updates instantly!',
+			name: 'Lonsdale Smith',
+			title: 'Designer',
+			image:
+				'https://images.generated.photos/-zjI21TA7IKXl-FTt2Vg9gbZAW89i0F-NkaqOLmxXFw/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTE2NzY4LmpwZw.jpg',
+		},
+	],
+]
+
+const carouselContainer = document.querySelector('.testimonial-carousel')
+
+testimonials.forEach((testimonial, index) => {
+	const item = document.createElement('div')
+	item.className =
+		'flex justify-center gap-4 md:gap-6 lg:gap-10 items-center flex-wrap min-w-full w-full snap-end '
+	item.id = `testimonial-item-${index + 1}`
+	testimonial.forEach((testimonialItem) => {
+		item.innerHTML += `<div class="size-64 lg:size-80 bg-gradient-to-tr from-white/10 to-white/20 rounded-2xl relative flex flex-col p-6 lg:px-10 lg:py-12 justify-center backdrop-blur-lg">
+					<div class="absolute top-0 left-0 border-t border-l border-purple-400 size-20 rounded-tl-2xl border-opacity-80"></div>
+					<div class="absolute bottom-0 right-0 border-b border-r size-20 border-purple-400 rounded-br-2xl border-opacity-80"></div>
+					<div class="flex gap-1">
+						${'<div class="mask mask-star-2 bg-yellow-500 size-4"></div>'.repeat(5)}
+					</div>
+					<blockquote class="text-sm lg:text-base font-semibold mt-4">${
+						testimonialItem.quote
+					}</blockquote>
+					<div class="flex items-center gap-2 mt-auto">
+						<div class="avatar">
+							<div class="w-12 rounded-full">
+								<img src="${testimonialItem.image}" alt="avatar of ${testimonialItem.name}" />
+							</div>
+						</div>
+						<div>
+							<h5 class="lg:text-lg">${testimonialItem.name}</h5>
+							<p class="!text-xs h6 lg:!text-sm text-neutral-300">${testimonialItem.title}</p>
+						</div>
+					</div>
+				</div>`
+	})
+	carouselContainer.appendChild(item)
+})
+
+const indicators = document.querySelectorAll('button[id^="indicator-"]')
+
+function setActiveSlide(index) {
+	// Scroll to the carousel item
+
+	document
+		.getElementById(`testimonial-item-${index}`)
+		.scrollIntoView({ behavior: 'smooth' })
+
+	// Update the active indicator
+	indicators.forEach((indicator, i) => {
+		indicator.classList.toggle('opacity-100', i === index - 1)
+		indicator.classList.toggle('opacity-50', i !== index - 1)
+	})
+}
